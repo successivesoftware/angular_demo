5b8ca0f50b74de118a33eccebc0a4cc44736a15a
successivesoftware <siddharth@successivesoftwares.com>
2015-10-07 18:58:02 +0530

    Changed Logo

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ var express = require('express');
 var path    = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname, '../ng-boilerplate/build')));
-app.use(express.static(path.join(__dirname, '../ng-boilerplate/')));
+app.use(express.static(path.join(__dirname, '../angular_demo/build')));
+app.use(express.static(path.join(__dirname, '../angular_demo/')));
 
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/../build/index.html'));
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8000, function () {
     var port = server.address().port;
     console.log('App listening at ' +  port);
 });
